854b8be8d69b901603344261d6ccf5fc9918849e
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-11-15 15:54:01 +0530

    Added Webscraper and completed webscrapping

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
+const puppeteer = require("puppeteer");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -13,9 +14,14 @@ app.set('view engine', 'ejs');
 mongoose.connect("mongodb://localhost:27017/customerDB",{useNewUrlParser:true,useUnifiedTopology:true});
 
 var defaultList = [];
+var productTitle = ['#productTitle','._35KyD6'];
+var productImage = ['#imgTagWrapperId > img','._1Nyybr'];
+var productPrice = ['#priceblock_ourprice','._1vC4OE','#priceblock_dealprice','._1vC4OE'];
 
 const orderSchema = new mongoose.Schema({
     productName:String,
+    url:String,
+    imageUrl:String,
     price: Number,
     expectedPrice:Number
 });
@@ -112,12 +118,20 @@ app.post("/signup",function(req,res){
 })
 
 app.post("/item/:user",function(req,res){
-    Customer.findOne({_email:req.params.user},function(err,foundCustomer){
+    Customer.findOne({_email:req.params.user},async function(err,foundCustomer){
         if(!err){
+            const optWebsite = parseInt(req.body.website);
+            var url = req.body.URL.split("ref=")[0];
+            url = url.split("?")[0];
+            console.log(url);
+            let data = await webScrapper(url,optWebsite);
+            console.log(data);
             var newOrder = new Order({
-                productName:req.body.URL,
-                price:req.body.userPrice,
-                expectedPrice:req.body.userPrice-20
+                productName: data.title,
+                url:url,
+                price:data.price,
+                imageUrl:data.image,
+                expectedPrice:req.body.userPrice
             });
             foundCustomer.orders.push(newOrder);
             foundCustomer.save();
@@ -140,3 +154,34 @@ app.post("/delete/:user",function(req,res){
 app.listen(3000, function(){
     console.log("Server is running on port 3000!!!");
 });
+
+async function webScrapper(url,optWebsite){
+    const browser = await puppeteer.launch({headless:true});
+    const page = await browser.newPage();
+    await page.goto(url,{timeout:1200000,waitForSelector:productImage[optWebsite]});
+    try{
+        let data = await page.evaluate((optWebsite,productTitle,productImage,productPrice)=>{
+            var title = document.querySelector(productTitle[optWebsite]).innerText;
+            var image = document.querySelector(productImage[optWebsite]).src;
+            var price;
+            var subElement = optWebsite==0 ? 7 : 1;
+            if(document.querySelector(productPrice[optWebsite]) != null){
+                price = parseFloat(document.querySelector(productPrice[optWebsite]).innerHTML.substring(subElement));
+            }else{
+                price = parseFloat(document.querySelector(productPrice[optWebsite+2]).innerHTML,substring(subElement));
+            }
+            return{
+                title,
+                image,
+                price
+            }
+        }, optWebsite,productTitle,productImage,productPrice);
+        return data;
+    }catch(e){
+        console.log("Error Happend ! Please check if you have opted the details correctly");
+        res.redirect("/userdashboard/"+foundCustomer._email);
+    }
+    finally{
+        browser.close();
+    }
+}
